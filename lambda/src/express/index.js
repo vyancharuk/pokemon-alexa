e9ff6a399f8fb5af2bb473209f8a583da6bfd045
Vladimir Profile on Mac <v.yancharuk@gmail.com>
2020-05-15 02:37:46 +0300

    remove trace middleware

--- lambda/src/express/index.js
+++ lambda/src/express/index.js
@@ -8,34 +8,13 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-// TODO: remove traceMidleware
-const traceMiddleware = (req, res, next) => {
-  const date = new Date();
-  const ts = `${date
-    .getHours()
-    .toString()
-    .padStart(2, '0')}:${date
-    .getMinutes()
-    .toString()
-    .padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
-
-  console.log(`[${ts}]traceMiddleware req.headers=`, req.headers);
-
-  req.on('data', (chunk) => {
-    const bodydata = chunk.toString('utf8');
-    console.log('bodydata =', JSON.parse(bodydata));
-  });
-
-  next();
-};
-
 const skillBuilder = initSkillBuilder();
 const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, true, true);
 
-app.post('/', traceMiddleware, adapter.getRequestHandlers());
+app.post('/', adapter.getRequestHandlers());
 
-app.listen(PORT, function (err) {
+app.listen(PORT, (err) => {
   if (err) {
     console.error(`Error during server startup ${err.toString()}`);
   }
